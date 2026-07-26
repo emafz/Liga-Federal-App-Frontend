88f400f0f2535edd6f94e112c09ef6b7ce8fc30f
Emanuel Fernandez <emafz0@gmail.com>
2026-07-26 20:00:39 -0300

    generar estructura de la aplicación frontend con estilos base, enrutamiento y módulos de servicio de API para la gestión de usuarios

---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,14 @@
+import Navigation from '@/components/blocks/Navigation/Navigation'
+
+import styles from './Register.module.css'
+
+function Register() {
+  return (
+    <main className={styles.container}>
+      <Navigation />
+      <h1>Hello Register</h1>
+    </main>
+  )
+}
+
+export default Register
